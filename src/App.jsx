e9a0498ab9d1faa08070335e59ed0f62b3fa8670
itxSaaad <saadstudent.cs@gmail.com>
2023-02-05 17:21:40 +0500

    Added: Vite BoilerPlate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List App - React</h1>
+      <p>
+        A Things To Do list App using React.Js to make you Remember your Todo's
+      </p>
+    </div>
+  );
+}
+
+export default App;
